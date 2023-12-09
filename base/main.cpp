c701084b5ceb86dfd836fb0cae7c6a41158fb437
Eftychios Karagiorgis <eftichios1997@hotmail.com>
2023-12-09 12:46:15 +0200

    Update base main.cpp

--- base/main.cpp
+++ base/main.cpp
@@ -1,7 +1,28 @@
-#include "../common/win32_utils.hpp"
+#include "../common/win32_utils.cpp"
+
+void ParseFileContents(char* fileContents, DWORD fileSize, MemoryArena* memoryArena) 
+{
+    // temp hack to avoid sequential memory usage in our string
+    // give some padding to our memory
+    AllocateMemory(memoryArena, 1);
+    return;
+}
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
                       LPSTR lpCmdLine, int nShowCmd)
 {
+    const char* fileName = "input.txt";
+    DWORD fileSize = GetFileSize(fileName);
+
+    MemoryArena memoryArena = {};
+    memoryArena.totalSize = fileSize + Kilobytes(100);
+    memoryArena.memory = VirtualAlloc(0, memoryArena.totalSize, 
+            MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+
+    FileResult fileResult = ReadTextFile(fileName, fileSize, &memoryArena);
+
+    ParseFileContents((char*)fileResult.result, fileSize, &memoryArena);
+
+    FreeAllMemory(&memoryArena);
     return 0;
 }
